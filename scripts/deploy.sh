1ff75a689f201f8c66451dae789ede2333495cce
Gayeon <922g@naver.com>
2024-02-29 20:17:59 +0900

    cicd : 한글 -> 영어로 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,12 +11,12 @@ CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 종료할것 없음."
+  echo "> nothing kill"
 else
   echo "> kill -9 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포"
+echo "> $JAR_PATH cd"
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
