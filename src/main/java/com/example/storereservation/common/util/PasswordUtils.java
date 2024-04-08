b19371a71db293a5d79926c6382578e97111f993
Gayeon <922g@naver.com>
2024-04-09 00:29:10 +0900

    feat : PasswordUtils 생성

---
+++ src/main/java/com/example/storereservation/common/util/PasswordUtils.java
@@ -0,0 +1,12 @@
+package com.example.storereservation.common.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PasswordUtils {
+
+    // TODO : 암호화
+    public static String encryptPassword(String password) {
+        return password;
+    }
+}
