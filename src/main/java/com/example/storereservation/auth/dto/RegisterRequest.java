bb312ad445f78e25f7a4cc8482efb23859ae72a6
Gayeon <922g@naver.com>
2024-05-23 19:24:05 +0900

    refactor : string 필드 길이 체크 -> @Length (#1)

--- src/main/java/com/example/storereservation/auth/dto/RegisterRequest.java
+++ src/main/java/com/example/storereservation/auth/dto/RegisterRequest.java
@@ -4,12 +4,12 @@ import com.example.storereservation.common.validator.Password;
 import com.example.storereservation.common.validator.Tel;
 import com.example.storereservation.member.entity.Member;
 import com.example.storereservation.member.type.Role;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
@@ -19,12 +19,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class RegisterRequest {
 
     @NotBlank(message = "이름은 필수입력값입니다.")
-    @Max(value = 20, message = "이름은 최대 20자까지 가능합니다")
+    @Length(max = 20, message = "이름은 최대 20자까지 가능합니다")
     private String name;
     @Tel
     private String phone;
     @NotBlank(message = "아이디는 필수입력값입니다.")
-    @Max(value = 20, message = "아이디는 최대 20자까지 가능합니다.")
+    @Length(max = 20, message = "아이디는 최대 20자까지 가능합니다.")
     private String userId;
     @Password
     private String password;
